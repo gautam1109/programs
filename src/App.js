729d2f11804ae1bea9b6f86ba37ed1e5e7875611
Gautam <gautam.111@gmail.com>
2022-08-11 06:06:15 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./styles.css";
-
+import {MaxSubArraySum} from "./programs/slidingwindow/maxSubArraySum"
 export default function App() {
   return (
     <div className="App">
       <h1>Learning</h1>
       <h2>All Programs</h2>
+      <MaxSubArraySum />
     </div>
   );
 }
--- src/programs/slidingwindow/maxSubArraySum.jsx
+++ src/programs/slidingwindow/maxSubArraySum.jsx
@@ -1,3 +1,35 @@
-const maxSubArraySum = () => {
-  return <></>;
+export const MaxSubArraySum = () => {
+  return (<>Hello</>);
 };
+
+export const maxSumNaive = (arr, num)=>{
+  if(num>arr.length) return null;
+  let max= -Infinity;
+  for(let i=0; i<arr.length-num+1; i++){
+    let temp=0;
+    for (let j = 0; j < num; j++) {
+      temp+=+arr[i+j];
+
+    }
+    max = Math.max(temp, max)
+  }
+  console.log("Naive:",max);
+  return max;
+}
+
+export const maxSumSmart = (arr, num)=>{
+  if(num>arr.length) return null;
+  let tempSum=0,maxSum=0;
+  for(let i=0; i<num; i++){
+    tempSum+=arr[i]
+  }
+  maxSum=tempSum;
+  for(let i=num;i<arr.length;i++){
+    tempSum=tempSum-arr[i-num]+arr[i]
+    maxSum = Math.max(tempSum, maxSum);
+  }
+  console.log("Smart:",maxSum);
+  return maxSum;
+}
+//maxSumNaive([3,2,4,7,1,2,9],2);
+maxSumSmart([3,2,5,7,1,1,9],2);
